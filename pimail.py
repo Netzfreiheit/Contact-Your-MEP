8d5fb6a39f367d9776af0988f252fa19a0a8e6e6
Michael Bauer <mihi@lo-res.org>
2013-12-18 20:48:25 +0100

    added templating etc

---
+++ pimail.py
@@ -0,0 +1,23 @@
+import web
+import json
+from jinja2 import Template
+import random
+
+urls= (
+  '/(.*)' , 'main' )
+
+with open("widget.tmpl") as f:
+  template = Template(f.read())
+
+app= web.application(urls,globals())
+with open("data.json") as f:
+  meps=json.load(f)
+
+class main:
+  def GET(self,foo):
+    web.header("Content-Type", "text/html;charset=utf-8")
+    m=random.choice(meps)
+    return template.render(m)
+
+if __name__=="__main__":
+  app.run()
