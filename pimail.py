2d79ebaa536ade4350e0e0bbf682c48df27e0d3f
Michael Bauer <mihi@lo-res.org>
2014-01-23 19:08:50 +0100

    cleanup

--- pimail.py
+++ pimail.py
@@ -3,15 +3,14 @@ import json
 from jinja2 import Template
 import random
 
-
-with open("widget.tmpl") as f:
-    template = Template(f.read().decode("utf-8"))
-
+" Load Data "
 with open("data.json") as f:
     meps = json.load(f)
 
 class mail:
+    """ Handle Requests for Mail """
     def GET(self):
+        """ Handle GET Requests """
         web.header("Content-Type", "text/html;charset=utf-8")
         with open("widget.tmpl") as f:
             template = Template(f.read().decode("utf-8"))
