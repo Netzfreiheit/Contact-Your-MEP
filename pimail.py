6286ca92f8804299c9c4deb4fa2eaa71f291ec59
Michael Bauer <mihi@lo-res.org>
2014-01-23 19:18:18 +0100

    fixes #1 weighted_choice weights based on score

--- pimail.py
+++ pimail.py
@@ -1,11 +1,23 @@
 import web
 import json
-from jinja2 import Template
 import random
+from jinja2 import Template
 
 " Load Data "
 with open("data.json") as f:
     meps = json.load(f)
+    total_score = sum((i['score'] for i in meps))
+
+def weighted_choice(a):
+    """ Pick a MEP based on the score weight """
+    r = random.uniform(0,total_score)
+    n = 0
+    for c in a:
+        n = n + c['score']
+        if n>r:
+            return c
+    return False
+
 
 class mail:
     """ Handle Requests for Mail """
@@ -14,7 +26,7 @@ class mail:
         web.header("Content-Type", "text/html;charset=utf-8")
         with open("widget.tmpl") as f:
             template = Template(f.read().decode("utf-8"))
-        m = random.choice(meps)
+        m = weighted_choice(meps)
         return template.render(m)
 
 urls = ('/', 'mail' )
