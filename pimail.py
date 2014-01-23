8115903c8951ff48312e0dd86a0596737d28bba9
Michael Bauer <mihi@lo-res.org>
2014-01-23 19:07:38 +0100

    refactored - pep8ify

--- pimail.py
+++ pimail.py
@@ -3,23 +3,24 @@ import json
 from jinja2 import Template
 import random
 
-urls= (
-  '/' , 'mail' )
 
 with open("widget.tmpl") as f:
-  template = Template(f.read().decode("utf-8"))
+    template = Template(f.read().decode("utf-8"))
 
-app= web.application(urls,globals())
 with open("data.json") as f:
-  meps=json.load(f)
+    meps = json.load(f)
 
 class mail:
-  def GET(self):
-    web.header("Content-Type", "text/html;charset=utf-8")
-    with open("widget.tmpl") as f:
-      template = Template(f.read().decode("utf-8"))
-    m=random.choice(meps)
-    return template.render(m)
+    def GET(self):
+        web.header("Content-Type", "text/html;charset=utf-8")
+        with open("widget.tmpl") as f:
+            template = Template(f.read().decode("utf-8"))
+        m = random.choice(meps)
+        return template.render(m)
 
-if __name__=="__main__":
-  app.run()
+urls = ('/', 'mail' )
+
+app = web.application(urls,globals())
+
+if __name__ == "__main__":
+    app.run()
