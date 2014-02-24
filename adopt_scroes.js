21db17a1c88990e7ffe92428f3e7ea3b1251ed6c
Thomas Lohninger <thomas@lohninger.org>
2014-02-24 11:20:10 +0100

    adopt scores

--- adopt_scroes.js
+++ adopt_scroes.js
@@ -20,22 +20,30 @@ var data = require('./data.json');
 for (var i = 0; i < data.length; i++) {
 	try {
 		if (data[i].name.indexOf('Trautmann') !== -1 || data[i].name.indexOf('Andersdotter') !== -1 ) {
-			data[i].score = 0.1;
+			data[i].score = 0.0;
 		} else if (data[i].role.indexOf('shadow') !== -1 || data[i].role.indexOf('rapporteur') !== -1 ) {
 			data[i].score = 0.9;
 		}
 		else if (data[i].group_short.indexOf('epp') !== -1 || data[i].group_short.indexOf('aldeadle') !== -1) {
-			if (data[i].role.indexOf('member') !== -1) {
+			if (data[i].name.toUpperCase().indexOf('JOHANSSON') !== -1 || data[i].name.toUpperCase().indexOf('ALABART') !== -1 || data[i].name.toUpperCase().indexOf('GUNNAR') !== -1 || data[i].name.toUpperCase().indexOf('BELET') !== -1 || data[i].name.toUpperCase().indexOf('BUZEK') !== -1 || data[i].name.toUpperCase().indexOf('EHLER') !== -1 || data[i].name.toUpperCase().indexOf('KELLY') !== -1 || data[i].name.toUpperCase().indexOf('LANGEN') !== -1 || data[i].name.toUpperCase().indexOf('NIEBLER') !== -1 || data[i].name.toUpperCase().indexOf('NISTELROOIJ') !== -1 || data[i].name.toUpperCase().indexOf('RÜBIG') !== -1 || data[i].name.toUpperCase().indexOf('WINKLER') !== -1 || data[i].name.toUpperCase().indexOf('JORDAN') !== -1) {
 				data[i].score = 0.8;
+			} else if (data[i].role.indexOf('member') !== -1) {
+				data[i].score = 0.6;
 			} else {
 				data[i].score = 0.5;
 			}
 		}
 		else if (data[i].group_short.indexOf('greensefa') !== -1) {
-			data[i].score = 0.25;
+			data[i].score = 0.1;
 		}
 		else if (data[i].group_short.indexOf('sd') !== -1) {
-			data[i].score = 0.50;
+			data[i].score = 0.2;
+		}
+		else if (data[i].group_short.indexOf('guengl') !== -1) {
+			data[i].score = 0.1;
+		}
+		else if (data[i].group_short.indexOf('efd') !== -1) {
+			data[i].score = 0.0;
 		}
 		else {
 			if (data[i].role.indexOf('member') !== -1) {
