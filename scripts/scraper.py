df43d21612b519d6befea6ab105268fe71c4a897
Thomas Lohninger <thomas@lohninger.org>
2015-03-09 02:33:43 +0100

    updated MEPs and scraper for new legislation

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -2,12 +2,14 @@ import lxml.html
 import json
 import urllib2
 from cookielib import CookieJar
+import re
 
 cj=CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
 base="http://www.europarl.europa.eu"
 list="http://www.europarl.europa.eu/meps/en/full-list.html?filter=all&leg="
+country_codes={'Malta':'mt','UnitedKingdom':'gb','Germany':'de','Italy':'it','France':'fr','Portugal':'pt','Sweden':'se','Spain':'es','Lithuania':'lt','Greece':'gr','Romania':'ro','Denmark':'dk','Ireland':'ie','Netherlands':'nl','Luxembourg':'lu','Hungary':'hu','Croatia':'hr','Slovakia':'sk','Austria':'at','Belgium':'be','Poland':'pl','Bulgaria':'bg','CzechRepublic':'cz','Finland':'fi','Slovenia':'si','Latvia':'lv','Cyprus':'cy','Estonia':'ee'}
 
 pages=[list]
 
@@ -24,18 +26,43 @@ def extract_urls_from_page(u):
 def expand_mep_info(m):
   print m['url']
   r=root_from_url(m['url'].replace(" ","%20"))
+  m['id']=re.match(r'.*\/meps\/en\/(\d+)\/', m['url']).group(1)
   m['image']="%s%s"%(base,r.xpath('//img[@class="photo_mep"]/@src')[0])
-  m['name']=" ".join(r.xpath('//li[@class="mep_name"]')[0].itertext())
+  m['name']=" ".join(r.xpath('//li[@class="mep_name"]')[0].itertext()).strip()
   info=r.xpath('//div[@class="zone_info_mep_transparent_mep_details"]')[0]
   m['group']=info.xpath('./ul/li[2]')[0].text.strip()
   m['group_short']=info.xpath('./ul/li[2]/@class')[0].split(" ",1)[1]
   m['party']=info.xpath('.//span[@class="name_pol_group"]')[0].text
   m['country']=info.xpath('./ul/li[3]')[0].text.strip()
-  m['country_short']=info.xpath('./ul/li[3]/@class')[0].split(" ",1)[1]
+  m['country_short']=country_codes[m['country'].replace(' ', '')]
   try:
     m['email']="".join(reversed(r.xpath('//a[@id="email-0"]/@href')[0].replace("mailto:","").replace("[dot]",".").replace("[at]","@")))
   except IndexError:
     m['email']=None
+
+  try:
+    m['fax_bxl']=re.sub(r"[\s\n\r]", "", r.xpath('//span[@class="fax"]')[0].text.replace("(0)", ""))
+  except IndexError:
+    m['fax_bxl']=None
+  try:
+    m['fax_stg']=re.sub(r"[\s\n\r]", "", r.xpath('//span[@class="fax"]')[1].text.replace("(0)", ""))
+  except IndexError:
+    m['fax_stg']=None
+
+  try:
+    m['twitter']=re.sub(r".*twitter.com\/", "", r.xpath('//a[@class="link_twitt"]/@href')[0])
+  except IndexError:
+    m['twitter']=None
+
+  try:
+    m['facebook']=r.xpath('//a[@class="link_fb"]/@href')[0]
+  except IndexError:
+    m['facebook']=None
+  try:
+    m['website']=r.xpath('//a[@class="link_website"]/@href')[0]
+  except IndexError:
+    m['website']=None
+
   return m
 
 def do():
