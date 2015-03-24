0fce2bf37b8a84908f8670393e24852fa7cc5343
Thomas Lohninger <thomas@lohninger.org>
2015-03-24 01:34:28 +0100

    adapt scores based on committee

--- scripts/adopt_scroes.js
+++ scripts/adopt_scroes.js
@@ -14,30 +14,48 @@
 			substitutes 0.4
 */
 
-
 var data = require('../data/data.json');
 
 for (var i = 0; i < data.length; i++) {
 	try {
 		if (data[i].group_short.indexOf('epp') !== -1) {
-			data[i].score = 0.8;
+			data[i].score = 0.4;
 		} else if (data[i].group_short.indexOf('aldeadle') !== -1) {
 			data[i].score = 0.3;
 		}
 		else if (data[i].group_short.indexOf('greensefa') !== -1) {
-			data[i].score = 0.1;
+			data[i].score = 0.15;
 		}
 		else if (data[i].group_short.indexOf('sd') !== -1) {
 			data[i].score = 0.3;
 		}
 		else if (data[i].group_short.indexOf('guengl') !== -1) {
-			data[i].score = 0.2;
+			data[i].score = 015;
 		}
 		else if (data[i].group_short.indexOf('efd') !== -1) {
-			data[i].score = 0.1;
+			data[i].score = 0.35;
+		}
+		else if (data[i].group_short.indexOf('ni') !== -1) {
+			data[i].score = 0.3;
 		}
 		else {
 			data[i].score = 0.1;
+		}
+
+		if ((data[i]['Members']||[]).indexOf('ITRE') != -1) {
+			data[i].score += 0.5;
+		} else if ((data[i]['Members']||[]).indexOf('IMCO') != -1) {
+			data[i].score += 0.4;
+		} else if ((data[i]['Substitute']||[]).indexOf('ITRE') != -1) {
+			data[i].score += 0.2;
+		} else if ((data[i]['Substitute']||[]).indexOf('IMCO') != -1) {
+			data[i].score += 0.15;
+		}
+
+		if (data[i].id == "28340") {
+			data[i].score=0.9;
+		} else if (data[i].id == "96710") {
+			data[i].score=0.9;
 		}
 	} catch (e) {
 		console.error('couldn\'t update score of MEP # ' + i + ' because of ', e);
