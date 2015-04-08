46724f68815ad09dba44cf02d5786a07feb35f2b
Thomas Lohninger <thomas@lohninger.org>
2015-04-08 17:51:51 +0200

    updated score
    updated db scheme

--- db-init.sql
+++ db-init.sql
@@ -2,9 +2,10 @@ CREATE TABLE IF NOT EXISTS faxes (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 	message text, 
 	faxnr varchar(120), 
-	create_date timestamp, 
 	status int default 0, /* 0:new, 1:queed, 2:send, 3:retry, -1: error*/
-	change_date timestamp, 
+	create_date timestamp DEFAULT CURRENT_TIMESTAMP, 
+	modify_date timestamp ON UPDATE CURRENT_TIMESTAMP, 
+	send_date timestamp, 
 	campaign_id	INT
 );
 
