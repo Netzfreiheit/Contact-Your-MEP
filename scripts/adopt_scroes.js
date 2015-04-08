0f6597cfc0797430f8428f710306cc4b1aaa4086
Thomas Lohninger <thomas@lohninger.org>
2015-04-08 17:58:46 +0200

    back to old scoring

--- scripts/adopt_scroes.js
+++ scripts/adopt_scroes.js
@@ -22,48 +22,48 @@ for (var i = 0; i < data.length; i++) {
 		if (data[i].group_short.indexOf('epp') !== -1) {
 			data[i].score = 0.4;
 		} else if (data[i].group_short.indexOf('aldeadle') !== -1) {
-			data[i].score = 0.7;
+			data[i].score = 0.3;
 		}
 		else if (data[i].group_short.indexOf('greensefa') !== -1) {
-			data[i].score = 0.25;
+			data[i].score = 0.15;
 		}
 		else if (data[i].group_short.indexOf('sd') !== -1) {
-			data[i].score = 0.7;
+			data[i].score = 0.3;
 		}
 		else if (data[i].group_short.indexOf('guengl') !== -1) {
-			data[i].score = 0.25;
+			data[i].score = 0.15;
 		}
 		else if (data[i].group_short.indexOf('efd') !== -1) {
-			data[i].score = 0.8;
+			data[i].score = 0.35;
 		}
 		else if (data[i].group_short.indexOf('ni') !== -1) {
-			data[i].score = 0.6;
+			data[i].score = 0.3;
 		}
 		else {
 			data[i].score = 0.1;
 		}
 
 		if ((data[i]['Members']||[]).indexOf('ITRE') != -1 || (data[i]['Chair']||[]).indexOf('ITRE') != -1 || (data[i]['Vice-Chair']||[]).indexOf('ITRE') != -1) {
-			data[i].score += 2.5;
+			data[i].score += 0.5;
 		} else if ((data[i]['Members']||[]).indexOf('IMCO') != -1) {
-			data[i].score += 2.4;
+			data[i].score += 0.4;
 		} else if ((data[i]['Substitute']||[]).indexOf('ITRE') != -1) {
-			data[i].score += 1.2;
+			data[i].score += 0.2;
 		} else if ((data[i]['Substitute']||[]).indexOf('IMCO') != -1) {
-			data[i].score += 1.15;
+			data[i].score += 0.15;
 		}
 
 		// shadows
 		if (data[i].id == "28340") {
-			data[i].score=3;
+			data[i].score=0.9;
 		} else if (data[i].id == "96710") {
-			data[i].score=3;
+			data[i].score=0.9;
 		} else if (data[i].id == "96820") {
-			data[i].score=3;
+			data[i].score=0.9;
 		} else if (data[i].id == "96949") {
-			data[i].score=3;
+			data[i].score=0.9;
 		} else if (data[i].id == "124813") {
-			data[i].score=3;
+			data[i].score=0.9;
 		}
 	} catch (e) {
 		console.error('couldn\'t update score of MEP # ' + i + ' because of ', e);
