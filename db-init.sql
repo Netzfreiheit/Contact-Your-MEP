4d7fdad2219029705348e0c74385e82adebd1969
Thomas Lohninger <thomas@lohninger.org>
2015-04-24 17:29:42 +0200

    mep table for overview and better statistics

--- db-init.sql
+++ db-init.sql
@@ -15,3 +15,21 @@ CREATE TABLE IF NOT EXISTS subscriptions (
 		country varchar(5), 
 		create_date timestamp
 );
+
+CREATE TABLE IF NOT EXISTS meps (
+	id INT UNSIGNED PRIMARY KEY, 
+	name varchar(255),
+	fax_bxl varchar(255),
+	fax_stg varchar(255),
+	country_short varchar(2),
+	group_short varchar(12),
+	fax_optout varchar(6)
+);
+
+#faxes/mep/day
+select m.name, day(f.send_date), count(*) as cnt from faxes as f join meps as m on (m.fax_bxl=f.faxnr) WHERE f.id>=144 and f.faxnr not in ('003222849473', '0033388179473', '003222849732', '0033388179732', '0033388179681', '003222849681') group by 1,2 order by 1,2;
+
+#avg nr of faxes /mep/day
+select avg(cnt) from 
+(select m.name, day(f.send_date), count(*) as cnt from faxes as f join meps as m on (m.fax_bxl=f.faxnr) WHERE f.id>=144 and f.faxnr not in ('003222849473', '0033388179473', '003222849732', '0033388179732', '0033388179681', '003222849681') group by 1,2 order by 1,2
+	) as x;
