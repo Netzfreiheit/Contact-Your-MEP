d3fb77e418bc0c54fd7d2e01c04a8537eef5dedd
Niklas Meinzer <mail@niklas-meinzer.de>
2015-09-28 19:23:34 +0200

    backend: Some dummy data gets rendered into table

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
 
 from flask import Flask
@@ -7,7 +8,20 @@ app = Flask(__name__)
 # Entry point: View all faxes as list
 @app.route('/')
 def list():
-    return render_template('list_view.html')
+    data = [
+        {
+            "text": "asdpjsapdo oaisj füjfds aüsdijf aüosdifj aüosdijf aosidf jaüsoidjf üaoisdf",
+            "checked": False,
+            "sent": False
+        },
+        {
+            "text": "asdpjsapdo oaisj füjfds aüsdijf aüosdifj aüosdijf aosidf jaüsoidjf üaoisdf",
+            "checked": True,
+            "sent": True
+        }
+
+    ]
+    return render_template('list_view.html', data=data)
 
 if __name__ == '__main__':
     app.debug = True
